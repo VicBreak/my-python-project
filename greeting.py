9e720a40f6e6f492e00d8bf8437855876a3b5350
VicBreak <obiudoka11@gmail.com>
2025-05-25 05:40:24 +0100

    Initial commit

---
+++ greeting.py
@@ -0,0 +1,8 @@
+# Ask for the user's name
+name = input("What is your name? ")
+
+# Ask for the user's favorite color
+color = input("What is your favorite color? ")
+
+# Print a personalized greeting message
+print(f"Hello, {name}! Your favorite color, {color}, is awesome!")
